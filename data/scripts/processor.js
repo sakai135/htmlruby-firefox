f0a091649e7eaf07e44db33f9046699c70cfd1db
sakai135 <sakai135@users.noreply.github.com>
2014-10-15 01:59:58 -0400

    redo

--- data/scripts/processor.js
+++ data/scripts/processor.js
@@ -15,91 +15,122 @@ function process() {
 
   stopObserver();
 
-  var dataset = new Array(rubies.length);
-  var multi = [];
-  var ruby, i, j, rts, rbw, rbl, rt, rtw, rtl, data, perChar, rps, rpl, iMax, jMax;
+  var dataset = [];
 
   console.log('collect');
 
-  for (i = 0; i < rubyCount; i++) {
-    ruby = rubies[i];
-    rbw = ruby.clientWidth;
-    rbl = ruby.textContent.trim().length;
-    rts = ruby.querySelectorAll('rt');
-    if (rts.length > 1) {
-      multi.push(i);
-      rtw = 0;
-      rtl = 0;
-      for (j = 0, jMax = rts.length; j < jMax; j++) {
-        rt = rts[j];
-        rtw += rt.clientWidth;
-        rtl += rt.textContent.trim().length;
-      }
-    } else {
-      rt = rts[0];
-      rtw = rt.clientWidth;
-      rtl = rt.textContent.trim().length;
-    }
-    rpl = 0;
-    rps = ruby.querySelectorAll('rp');
-    for (j = 0, jMax = rps.length; j < jMax; j++) {
-      rpl += rps[j].textContent.trim().length;
-    } 
-    dataset[i] = [rtw, rtl, rbw, rbl - rtl - rpl, rts];
-  }
+  for (let i = 0; i < rubyCount; i++) {
+    let ruby = rubies[i];
+    let rtElems = ruby.querySelectorAll('rt');
+    let rpElems = ruby.querySelectorAll('rp');
+    let rbWidth = ruby.clientWidth;
+    let rubyChars = ruby.textContent.trim().length;
+    let groups = [];
+    let gElems = [];
+    let gWidth = 0;
+    let gChars = 0;
+    let rpChars = 0;
+    let rtChars = 0;
 
+    for (let j = 0, jMax = rtElems.length; j < jMax; j++) {
+      let rt = rtElems[j];
+      if (gWidth !== 0 && (rt.previousElementSibling === null || rt.previousElementSibling.nodeName.toLowerCase() !== 'rt')) {
+        rtChars += gChars;
+        groups.push([gWidth, gChars, gElems, 0]);
+        gWidth = 0;
+        gChars = 0;
+        gElems = [];
+      }
+      gWidth += rt.clientWidth;
+      gChars += rt.textContent.trim().length;
+      gElems.push([rt, 0]);
+    }
+    rtChars += gChars;
+    groups.push([gWidth, gChars, gElems, 0]);
+
+    for (let j = 0, jMax = rpElems.length; j < jMax; j++) {
+      rpChars += rpElems[j].textContent.trim.length;
+    }
+
+    dataset.push([rbWidth, rubyChars - rtChars - rpChars, groups]);
+  }
+  
   console.log('space');
 
-  for (i = 0; i < rubyCount; i++) {
-    ruby = rubies[i];
-    data = dataset[i];
-    rtw = data[0];
-    rtl = data[1];
-    rbw = data[2];
-    rbl = data[3];
-    rts = data[4];
-    if (rbw > rtw) {
-      perChar = (rbw - rtw) / rtl;
+  for (let i = 0; i < rubyCount; i++) {
+    let ruby = rubies[i];
+    let data = dataset[i];
+    let rbWidth = data[0];
+    let rbChars = data[1];
+    let groups = data[2];
+    let maxWidth = rbWidth;
 
-      ruby.style.maxWidth = rbw + 'px';
-      for (j = 0, jMax = rts.length; j < jMax; j++) {
-        rt = rts[j];
-        rt.style.letterSpacing = perChar + 'px';
-        rt.style.left = (j > 0 ? perChar : (perChar / 2)) + 'px';
+    for (let j = 0, jMax = groups.length; j < jMax; j++) {
+      let group = groups[j];
+      if (group[0] > maxWidth) {
+        maxWidth = group[0];
       }
-    } else {
-      perChar = (rtw - rbw) / rbl;
+    }
 
-      ruby.style.maxWidth = rtw + 'px';
+    if (maxWidth > rbWidth) {
+      let perChar = (maxWidth - rbWidth) / rbChars;
       ruby.style.letterSpacing = perChar + 'px';
-      ruby.style.textIndent = perChar / 2 + 'px';
+      ruby.style.textIndent = (perChar / 2) + 'px';
+    }
+
+    for (let j = 0, jMax = groups.length; j < jMax; j++) {
+      let group = groups[j];
+      let gWidth = group[0];
+      
+      if (maxWidth > gWidth) {
+        let gElems = group[2];
+        let perChar = (rbWidth - gWidth) / group[1];
+
+        for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+          let rt = gElems[k][0];
+          group[3] = perChar;
+          rt.style.letterSpacing = perChar + 'px';
+        }
+      }
     }
   }
 
-  console.log('multi');
+  console.log('position');
 
-  rtw = new Array(multi.length);
-  for (i = 0, iMax = multi.length; i < iMax; i++) {
-    rts = dataset[multi[i]][4];
-    rtl = new Array(rts.length);
-    rbw = 0;
-    for (j = 0, jMax = rts.length; j < jMax; j++) {
-      rtl[j] = rbw;
-      rbw += rts[j].clientWidth;
+  for (let i = 0; i < rubyCount; i++) {
+    let data = dataset[i];
+    let groups = data[2];
+
+    for (let j = 0, jMax = groups.length; j < jMax; j++) {
+      let group = groups[j];
+      let perChar = group[3];
+      let gElems = group[2];
+      let offset = 0;
+
+      for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+        let elem = gElems[k];
+        gElems[k][1] = offset + (perChar / 2);
+        offset += gElems[k][0].clientWidth;
+      }
     }
-    rtw[i] = rtl;
   }
-  for (i = 0, iMax = multi.length; i < iMax; i++) {
-    rts = dataset[multi[i]][4];
-    rtl = rtw[i];
-    for (j = 0, jMax = rts.length; j < jMax; j++) {
-      rts[j].style.marginRight = rtl[j] + 'px';
+  for (let i = 0; i < rubyCount; i++) {
+    let data = dataset[i];
+    let groups = data[2];
+
+    for (let j = 0, jMax = groups.length; j < jMax; j++) {
+      let group = groups[j];
+      let gElems = group[2];
+
+      for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+        gElems[k][0].style.marginLeft = gElems[k][1] + 'px';
+      }
     }
   }
 
   console.log('mark');
 
-  for (i = 0; i < rubyCount; i++) {
+  for (let i = 0; i < rubyCount; i++) {
     rubies[i].setAttribute('hr-processed', 1);
   }
 
