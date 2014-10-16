69e97f4ae78c402a81a69ad8ca3cf85c5127c52d
sakai135 <sakai135@users.noreply.github.com>
2014-10-15 23:07:14 -0400

    make shortcuts

--- data/scripts/processor.js
+++ data/scripts/processor.js
@@ -1,11 +1,12 @@
-"use strict";
+'use strict';
 
 var observer;
 
-function process() {
+function process(prefs) {
   console.log('process() start');
 
-  var rubies = document.body.querySelectorAll('ruby:not([hr-processed])');
+  var rubySelector = prefs.processInsertedContent ? 'ruby:not([hr-processed])' : 'ruby';
+  var rubies = document.body.querySelectorAll(rubySelector);
   var rubyCount = rubies.length;
 
   if (rubyCount < 1) {
@@ -13,30 +14,48 @@ function process() {
     return;
   }
 
-  stopObserver();
+  if (prefs.processInsertedContent) {
+    stopObserver();
+  }
 
+  var isSegmented = !!document.body.querySelector(rubySelector + ' rt + rt');
+  var isMulti = !!document.body.querySelector(rubySelector + ' rtc:nth-of-type(2)');
   var dataset = [];
 
-  console.log('collect');
+  console.log('collect [isSegmented:' + isSegmented + ', isMulti:' + isMulti + ']');
 
   for (let i = 0; i < rubyCount; i++) {
     let ruby = rubies[i];
     let rtElems = ruby.querySelectorAll('rt');
+    let rtCount = rtElems.length;
+
+    if (rtCount < 1) {
+      dataset.push(false);
+      continue;
+    }
+
+    let rt = rtElems[0];
+
+    if (!prefs.spaceRubyText && !isSegmented) {
+      dataset.push([0, 0, [0, 0, [[rt, 0]]]]);
+      continue;
+    }
+
     let rpElems = ruby.querySelectorAll('rp');
     let rbWidth = ruby.clientWidth;
     let rubyChars = ruby.textContent.trim().length;
     let groups = [];
-    let gElems = [];
-    let gWidth = 0;
-    let gChars = 0;
+    let gElems = [[rt, 0]];
+    let gWidth = rt.clientWidth;
+    let gChars = rt.textContent.trim().length;
     let rpChars = 0;
     let rtChars = 0;
 
-    for (let j = 0, jMax = rtElems.length; j < jMax; j++) {
-      let rt = rtElems[j];
-      if (gWidth !== 0 && (rt.previousElementSibling === null || rt.previousElementSibling.nodeName.toLowerCase() !== 'rt')) {
+    for (let j = 1, jMax = rtCount; j < jMax; j++) {
+      rt = rtElems[j];
+      if (rt.previousElementSibling === null || rt.previousElementSibling.nodeName.toLowerCase() !== 'rt') {
         rtChars += gChars;
-        groups.push([gWidth, gChars, gElems, 0]);
+        groups.push([gWidth, gChars, gElems]);
         gWidth = 0;
         gChars = 0;
         gElems = [];
@@ -46,95 +65,114 @@ function process() {
       gElems.push([rt, 0]);
     }
     rtChars += gChars;
-    groups.push([gWidth, gChars, gElems, 0]);
+    groups.push([gWidth, gChars, gElems]);
 
     for (let j = 0, jMax = rpElems.length; j < jMax; j++) {
-      rpChars += rpElems[j].textContent.trim.length;
+      rpChars += rpElems[j].textContent.trim().length;
     }
 
     dataset.push([rbWidth, rubyChars - rtChars - rpChars, groups]);
   }
   
-  console.log('space');
+  if (prefs.spaceRubyText) {
+    console.log('space');
 
-  for (let i = 0; i < rubyCount; i++) {
-    let ruby = rubies[i];
-    let data = dataset[i];
-    let rbWidth = data[0];
-    let rbChars = data[1];
-    let groups = data[2];
-    let maxWidth = rbWidth;
+    for (let i = 0; i < rubyCount; i++) {
+      let data = dataset[i];
 
-    for (let j = 0, jMax = groups.length; j < jMax; j++) {
-      let group = groups[j];
-      if (group[0] > maxWidth) {
-        maxWidth = group[0];
+      if (!data) {
+        continue;
       }
-    }
 
-    if (maxWidth > rbWidth) {
-      let perChar = (maxWidth - rbWidth) / rbChars;
-      ruby.style.letterSpacing = perChar + 'px';
-      ruby.style.textIndent = (perChar / 2) + 'px';
-    }
+      let ruby = rubies[i];
+      let rbWidth = data[0];
+      let rbChars = data[1];
+      let groups = data[2];
+      let maxWidth = rbWidth;
 
-    for (let j = 0, jMax = groups.length; j < jMax; j++) {
-      let group = groups[j];
-      let gWidth = group[0];
-      
-      if (maxWidth > gWidth) {
-        let gElems = group[2];
-        let perChar = (rbWidth - gWidth) / group[1];
+      for (let j = 0, jMax = groups.length; j < jMax; j++) {
+        let group = groups[j];
+        if (group[0] > maxWidth) {
+          maxWidth = group[0];
+        }
+      }
 
-        for (let k = 0, kMax = gElems.length; k < kMax; k++) {
-          let rt = gElems[k][0];
-          group[3] = perChar;
-          rt.style.letterSpacing = perChar + 'px';
+      if (maxWidth > rbWidth) {
+        ruby.style.width = maxWidth + 'px';
+        if (rbChars > 1) {
+          let perChar = (maxWidth - rbWidth) / rbChars;
+          ruby.style.letterSpacing = perChar + 'px';
+          ruby.style.textIndent = (perChar / 2) + 'px';
+        }
+      }
+
+      for (let j = 0, jMax = groups.length; j < jMax; j++) {
+        let group = groups[j];
+        let gWidth = group[0];
+        let gChars = group[1];
+        
+        if (maxWidth > gWidth) {
+          let gElems = group[2];
+          if (gChars === 1) {
+            gElems[0][0].style.width = maxWidth + 'px';
+          } else {
+            let perChar = (maxWidth - gWidth) / gChars;
+
+            for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+              let rt = gElems[k][0];
+              rt.style.letterSpacing = perChar + 'px';
+              rt.style.textIndent = (perChar / 2) + 'px';
+            }
+          }
         }
       }
     }
   }
 
-  console.log('position');
+  if (isSegmented) {
+    console.log('position');
 
-  for (let i = 0; i < rubyCount; i++) {
-    let data = dataset[i];
-    let groups = data[2];
+    for (let i = 0; i < rubyCount; i++) {
+      let data = dataset[i];
+      let groups = data[2];
 
-    for (let j = 0, jMax = groups.length; j < jMax; j++) {
-      let group = groups[j];
-      let perChar = group[3];
-      let gElems = group[2];
-      let offset = 0;
+      for (let j = 0, jMax = groups.length; j < jMax; j++) {
+        let group = groups[j];
+        let gElems = group[2];
+        let offset = 0;
 
-      for (let k = 0, kMax = gElems.length; k < kMax; k++) {
-        let elem = gElems[k];
-        gElems[k][1] = offset + (perChar / 2);
-        offset += gElems[k][0].clientWidth;
+        for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+          let elem = gElems[k];
+          elem[1] = offset;
+          offset += elem[0].clientWidth;
+        }
+      }
+    }
+    for (let i = 0; i < rubyCount; i++) {
+      let data = dataset[i];
+      let groups = data[2];
+
+      for (let j = 0, jMax = groups.length; j < jMax; j++) {
+        let group = groups[j];
+        let gElems = group[2];
+
+        for (let k = 0, kMax = gElems.length; k < kMax; k++) {
+          let elem = gElems[k];
+          elem[0].style.marginLeft = elem[1] + 'px';
+        }
       }
     }
   }
-  for (let i = 0; i < rubyCount; i++) {
-    let data = dataset[i];
-    let groups = data[2];
 
-    for (let j = 0, jMax = groups.length; j < jMax; j++) {
-      let group = groups[j];
-      let gElems = group[2];
+  if (prefs.processInsertedContent) {
+    console.log('mark');
 
-      for (let k = 0, kMax = gElems.length; k < kMax; k++) {
-        gElems[k][0].style.marginLeft = gElems[k][1] + 'px';
-      }
+    for (let i = 0; i < rubyCount; i++) {
+      rubies[i].setAttribute('hr-processed', 1);
     }
+
+    startObserver();
   }
-
-  console.log('mark');
-
-  for (let i = 0; i < rubyCount; i++) {
-    rubies[i].setAttribute('hr-processed', 1);
-  }
-
-  startObserver();
 
   console.log('process() end');
 }
@@ -169,24 +207,25 @@ function register() {
   }
 
   observer = new MutationObserver(onMutations);
-  startObserver();
 
   console.log('register() end');
 }
 
 function startObserver() {
-  if (observer) {
-    console.log('starting observer');
-
-    observer.observe(document.body, {
-      childList: true,
-      attributes: false,
-      characterData: false,
-      subtree: true
-    });
-
-    console.log('started observer');
+  if (!observer) {
+    register();    
   }
+
+  console.log('starting observer');
+
+  observer.observe(document.body, {
+    childList: true,
+    attributes: false,
+    characterData: false,
+    subtree: true
+  });
+
+  console.log('started observer');
 }
 
 function stopObserver() {
@@ -198,3 +237,6 @@ function stopObserver() {
     console.log('stopped observer');
   }
 }
+
+console.info(self.options);
+process(self.options);
